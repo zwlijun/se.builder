76c7014b8d1b0fecc0b23f82736733a5bf4c199b
zwlijun <zwlijun@hotmail.com>
2018-10-25 19:47:28 +0800

    调整 openshare.js

--- static/basic/src/js/mod/open/openshare.js
+++ static/basic/src/js/mod/open/openshare.js
@@ -22,217 +22,217 @@
             "qzone": {
                 "type": "redirect",
                 "url": 'http://sns.qzone.qq.com/cgi-bin/qzshare/cgi_qzshare_onekey',
-                "data": 'url=<%=openshare.link%>&title=<%=openshare.link%>&desc=<%=openshare.description%>&summary=<%=openshare.summary%>&site=<%=openshare.source%>'
+                "data": 'url=<%=encodeURIComponent(openshare.link)%>&title=<%=encodeURIComponent(openshare.link)%>&desc=<%=encodeURIComponent(openshare.description)%>&summary=<%=encodeURIComponent(openshare.summary)%>&site=<%=encodeURIComponent(openshare.source)%>'
             },
             "qq": {
                 "type": "redirect",
                 "url": 'http://connect.qq.com/widget/shareqq/index.html',
-                "data": 'url=<%=openshare.link%>&title=<%=openshare.title%>&source=<%=openshare.source%>&desc=<%=openshare.description%>'
+                "data": 'url=<%=encodeURIComponent(openshare.link)%>&title=<%=encodeURIComponent(openshare.title)%>&source=<%=encodeURIComponent(openshare.source)%>&desc=<%=encodeURIComponent(openshare.description)%>'
             },
             "tqq": {
                 "type": "redirect",
                 "url": 'http://share.v.t.qq.com/index.php',
-                "data": 'c=share&a=index&title=<%=openshare.description%>&url=<%=openshare.link%>&pic=<%=openshare.image%>'
+                "data": 'c=share&a=index&title=<%=encodeURIComponent(openshare.description)%>&url=<%=encodeURIComponent(openshare.link)%>&pic=<%=encodeURIComponent(openshare.image)%>'
             },
             "weibo": {
                 "type": "redirect",
                 "url": 'http://service.weibo.com/share/share.php',
-                "data": 'url=<%=openshare.link%>&title=<%=openshare.description%>&pic=<%=openshare.image%>&appkey=<%=openshare.appkey%>'
+                "data": 'url=<%=encodeURIComponent(openshare.link)%>&title=<%=encodeURIComponent(openshare.description)%>&pic=<%=encodeURIComponent(openshare.image)%>&appkey=<%=encodeURIComponent(openshare.appkey)%>'
             },
             "douban": {
                 "type": "redirect",
                 "url": 'http://shuo.douban.com/!service/share',
-                "data": 'href=<%=openshare.link%>&name=<%=openshare.title%>&text=<%=openshare.description%>&image=<%=openshare.image%>&starid=0&aid=0&style=11'
+                "data": 'href=<%=encodeURIComponent(openshare.link)%>&name=<%=encodeURIComponent(openshare.title)%>&text=<%=encodeURIComponent(openshare.description)%>&image=<%=encodeURIComponent(openshare.image)%>&starid=0&aid=0&style=11'
             },
             "diandian": {
                 "type": "redirect",
                 "url": 'http://www.diandian.com/share',
-                "data": 'lo=<%=openshare.link%>&ti=<%=openshare.description%>&type=link'
+                "data": 'lo=<%=encodeURIComponent(openshare.link)%>&ti=<%=encodeURIComponent(openshare.description)%>&type=link'
             },
             "xing": {
                 "type": "redirect",
                 "url": 'http://widget.renren.com/dialog/share',
-                "data": 'resourceUrl=<%=openshare.link%>&srcUrl=<%=openshare.link%>&title=<%=openshare.title%>&description=<%=openshare.description%>'
+                "data": 'resourceUrl=<%=encodeURIComponent(openshare.link)%>&srcUrl=<%=encodeURIComponent(openshare.link)%>&title=<%=encodeURIComponent(openshare.title)%>&description=<%=encodeURIComponent(openshare.description)%>'
             },
             "renren": {
                 "type": "redirect",
                 "url": 'https://www.xing.com/app/user',
-                "data": 'op=share&url=<%=openshare.link%>'
+                "data": 'op=share&url=<%=encodeURIComponent(openshare.link)%>'
             },
             "linkedin": {
                 "type": "redirect",
                 "url": 'http://www.linkedin.com/shareArticle',
-                "data": 'mini=true&title=<%=openshare.title%>&url=<%=openshare.link%>&summary=<%=openshare.summary%>&source=<%=openshare.source%>'
+                "data": 'mini=true&title=<%=encodeURIComponent(openshare.title)%>&url=<%=encodeURIComponent(openshare.link)%>&summary=<%=encodeURIComponent(openshare.summary)%>&source=<%=encodeURIComponent(openshare.source)%>'
             },
             "facebook": {
                 "type": "redirect",
                 "url": 'https://www.facebook.com/sharer/sharer.php',
-                "data": 'u=<%=openshare.link%>'
+                "data": 'u=<%=encodeURIComponent(openshare.link)%>'
             },
             "twitter": {
                 "type": "redirect",
                 "url": 'https://twitter.com/intent/tweet',
-                "data": 'text=<%=openshare.description%>&url=<%=openshare.link%>&via=<%=openshare.usrid || openshare.source%>&hashtags=<%=openshare.hash%>'
+                "data": 'text=<%=encodeURIComponent(openshare.description)%>&url=<%=encodeURIComponent(openshare.link)%>&via=<%=openshare.usrid || openshare.source%>&hashtags=<%=encodeURIComponent(openshare.hash)%>'
             },
             "google": {
                 "type": "redirect",
                 "url": 'https://plus.google.com/share',
-                "data": 'url=<%=openshare.link%>&text=<%=openshare.description%>&hl=<%=openshare.lang%>'
+                "data": 'url=<%=encodeURIComponent(openshare.link)%>&text=<%=encodeURIComponent(openshare.description)%>&hl=<%=encodeURIComponent(openshare.lang)%>'
             },
             "googlebookmarks": {
                 "type": "redirect",
                 "url": 'https://www.google.com/bookmarks/mark',
-                "data": 'op=edit&bkmk<%=openshare.link%>&title=<%=openshare.title%>&annotation=<%=openshare.description%>&labels=<%=openshare.hash%>'
+                "data": 'op=edit&bkmk<%=encodeURIComponent(openshare.link)%>&title=<%=encodeURIComponent(openshare.title)%>&annotation=<%=encodeURIComponent(openshare.description)%>&labels=<%=encodeURIComponent(openshare.hash)%>'
             },
             "reddit": {
                 "type": "redirect",
                 "url": "https://reddit.com/submit",
-                "data": "url=<%=openshare.link%>&title=<%=openshare.title%>"
+                "data": "url=<%=encodeURIComponent(openshare.link)%>&title=<%=encodeURIComponent(openshare.title)%>"
             },
             "tumblr": {
                 "type": "redirect",
                 "url": "https://www.tumblr.com/widgets/share/tool",
-                "data": "canonicalUrl=<%=openshare.link%>&title=<%=openshare.title%>&caption=<%=openshare.description%>&tags=<%=openshare.hash%>"
+                "data": "canonicalUrl=<%=encodeURIComponent(openshare.link)%>&title=<%=encodeURIComponent(openshare.title)%>&caption=<%=encodeURIComponent(openshare.description)%>&tags=<%=encodeURIComponent(openshare.hash)%>"
             },
             "pinterest": {
                 "type": "redirect",
                 "url": "http://pinterest.com/pin/create/link/",
-                "data": "url=<%=openshare.link%>"
+                "data": "url=<%=encodeURIComponent(openshare.link)%>"
             },
             "pinterestbutton": {
                 "type": "redirect",
                 "url": "http://pinterest.com/pin/create/button/",
-                "data": "url=<%=openshare.link%>"
+                "data": "url=<%=encodeURIComponent(openshare.link)%>"
             },
             "blogger": {
                 "type": "redirect",
                 "url": "https://www.blogger.com/blog-this.g",
-                "data": "u=<%=openshare.link%>&n=<%=openshare.title%>&t=<%=openshare.description%>"
+                "data": "u=<%=encodeURIComponent(openshare.link)%>&n=<%=encodeURIComponent(openshare.title)%>&t=<%=encodeURIComponent(openshare.description)%>"
             },
             "livejournal": {
                 "type": "redirect",
                 "url": "http://www.livejournal.com/update.bml",
-                "data": "event=<%=openshare.link%>&subject=<%=openshare.title%>"
+                "data": "event=<%=encodeURIComponent(openshare.link)%>&subject=<%=encodeURIComponent(openshare.title)%>"
             },
             "evernote": {
                 "type": "redirect",
                 "url": "http://www.evernote.com/clip.action",
-                "data": "url=<%=openshare.link%>&title=<%=openshare.title%>"
+                "data": "url=<%=encodeURIComponent(openshare.link)%>&title=<%=encodeURIComponent(openshare.title)%>"
             },
             "addthis": {
                 "type": "redirect",
                 "url": "http://www.addthis.com/bookmark.php",
-                "data": "url=<%=openshare.link%>"
+                "data": "url=<%=encodeURIComponent(openshare.link)%>"
             },
             "getpocket": {
                 "type": "redirect",
                 "url": "https://getpocket.com/edit",
-                "data": "url=<%=openshare.link%>"
+                "data": "url=<%=encodeURIComponent(openshare.link)%>"
             },
             "hackernews": {
                 "type": "redirect",
                 "url": "https://news.ycombinator.com/submitlink",
-                "data": "u=<%=openshare.link%>&t=<%=openshare.title%>"
+                "data": "u=<%=encodeURIComponent(openshare.link)%>&t=<%=encodeURIComponent(openshare.title)%>"
             },
             "stumbleupon": {
                 "type": "redirect",
                 "url": "http://www.stumbleupon.com/submit",
-                "data": "url=<%=openshare.link%>"
+                "data": "url=<%=encodeURIComponent(openshare.link)%>"
             },
             "digg": {
                 "type": "redirect",
                 "url": "http://digg.com/submit",
-                "data": "url=<%=openshare.link%>"
+                "data": "url=<%=encodeURIComponent(openshare.link)%>"
             },
             "buffer": {
                 "type": "redirect",
                 "url": "https://buffer.com/add",
-                "data": "url=<%=openshare.link%>&text=<%=openshare.title%>"
+                "data": "url=<%=encodeURIComponent(openshare.link)%>&text=<%=encodeURIComponent(openshare.title)%>"
             },
             "flipboard": {
                 "type": "redirect",
                 "url": "https://share.flipboard.com/bookmarklet/popout",
-                "data": "v=2&title=<%=openshare.title%>&url=<%=openshare.link%>"
+                "data": "v=2&title=<%=encodeURIComponent(openshare.title)%>&url=<%=encodeURIComponent(openshare.link)%>"
             },
             "instapaper": {
                 "type": "redirect",
                 "url": "http://www.instapaper.com/edit",
-                "data": "description=<%=openshare.description%>&title=<%=openshare.title%>&url=<%=openshare.link%>"
+                "data": "description=<%=encodeURIComponent(openshare.description)%>&title=<%=encodeURIComponent(openshare.title)%>&url=<%=encodeURIComponent(openshare.link)%>"
             },
             "surfingbird": {
                 "type": "redirect",
                 "url": "http://surfingbird.ru/share",
-                "data": "description=<%=openshare.description%>&title=<%=openshare.title%>&url=<%=openshare.link%>&screenshot=<%=openshare.image%>"
+                "data": "description=<%=encodeURIComponent(openshare.description)%>&title=<%=encodeURIComponent(openshare.title)%>&url=<%=encodeURIComponent(openshare.link)%>&screenshot=<%=encodeURIComponent(openshare.image)%>"
             },
             "flattr": {
                 "type": "redirect",
                 "url": "https://flattr.com/submit/auto",
-                "data": "hidden=HIDDEN&description=<%=openshare.description%>&title=<%=openshare.title%>&url=<%=openshare.link%>&language=<%=openshare.lang%>&tags=<%=openshare.hash%>&category=<%=openshare.category%>&user_id=<%=openshare.usrid || openshare.source%>"
+                "data": "hidden=HIDDEN&description=<%=encodeURIComponent(openshare.description)%>&title=<%=encodeURIComponent(openshare.title)%>&url=<%=encodeURIComponent(openshare.link)%>&language=<%=encodeURIComponent(openshare.lang)%>&tags=<%=encodeURIComponent(openshare.hash)%>&category=<%=encodeURIComponent(openshare.category)%>&user_id=<%=openshare.usrid || openshare.source%>"
             },
             "diaspora": {
                 "type": "redirect",
                 "url": "https://share.diasporafoundation.org/",
-                "data": "title=<%=openshare.title%>&url=<%=openshare.link%>"
+                "data": "title=<%=encodeURIComponent(openshare.title)%>&url=<%=encodeURIComponent(openshare.link)%>"
             },
             "vk": {
                 "type": "redirect",
                 "url": "http://vk.com/share.php",
-                "data": "title=<%=openshare.title%>&url=<%=openshare.link%>&comment=<%=openshare.description%>"
+                "data": "title=<%=encodeURIComponent(openshare.title)%>&url=<%=encodeURIComponent(openshare.link)%>&comment=<%=encodeURIComponent(openshare.description)%>"
             },
             "okru": {
                 "type": "redirect",
                 "url": "https://connect.ok.ru/dk",
-                "data": "st.cmd=WidgetSharePreview&st.shareUrl=<%=openshare.link%>"
+                "data": "st.cmd=WidgetSharePreview&st.shareUrl=<%=encodeURIComponent(openshare.link)%>"
             },
             "threema": {
                 "type": "redirect",
                 "url": "threema://compose",
-                "data": "text=<%=openshare.link%>&id=<%=openshare.usrid%>"
+                "data": "text=<%=encodeURIComponent(openshare.link)%>&id=<%=encodeURIComponent(openshare.usrid)%>"
             },
             "sms": {
                 "type": "redirect",
-                "url": "sms:<%=openshare.phone%>",
-                "data": "body=<%=openshare.description%>"
+                "url": "sms:<%=encodeURIComponent(openshare.phone)%>",
+                "data": "body=<%=encodeURIComponent(openshare.description)%>"
             },
             "lineit": {
                 "type": "redirect",
                 "url": "https://lineit.line.me/share/ui",
-                "data": "url=<%=openshare.link%>&text=<%=openshare.description%>"
+                "data": "url=<%=encodeURIComponent(openshare.link)%>&text=<%=encodeURIComponent(openshare.description)%>"
             },
             "line": {
                 "type": "redirect",
                 "url": "https://social-plugins.line.me/lineit/share",
-                "data": "url=<%=openshare.link%>&text=<%=openshare.description%>"
+                "data": "url=<%=encodeURIComponent(openshare.link)%>&text=<%=encodeURIComponent(openshare.description)%>"
             },
             "skype": {
                 "type": "redirect",
                 "url": "https://web.skype.com/share",
-                "data": "url=<%=openshare.link%>&text=<%=openshare.description%>"
+                "data": "url=<%=encodeURIComponent(openshare.link)%>&text=<%=encodeURIComponent(openshare.description)%>"
             },
             "telegram": {
                 "type": "redirect",
                 "url": "https://telegram.me/share/url",
-                "data": "url=<%=openshare.link%>&text=<%=openshare.title%>&to=<%=openshare.phone%>"
+                "data": "url=<%=encodeURIComponent(openshare.link)%>&text=<%=encodeURIComponent(openshare.title)%>&to=<%=encodeURIComponent(openshare.phone)%>"
             },
             "tgschema": {
                 "type": "redirect",
                 "url": "tg://msg",
-                "data": "url=<%=openshare.link%>&text=<%=openshare.title%>&to=<%=openshare.phone%>"
+                "data": "url=<%=encodeURIComponent(openshare.link)%>&text=<%=encodeURIComponent(openshare.title)%>&to=<%=encodeURIComponent(openshare.phone)%>"
             },
             "email": {
                 "type": "redirect",
-                "url": "mailto:<%=openshare.email%>",
-                "data": "subject=<%=openshare.title%>&body=<%=openshare.description%>"
+                "url": "mailto:<%=encodeURIComponent(openshare.email)%>",
+                "data": "subject=<%=encodeURIComponent(openshare.title)%>&body=<%=encodeURIComponent(openshare.description)%>"
             },
             "gmail": {
                 "type": "redirect",
                 "url": "https://mail.google.com/mail/",
-                "data": "view=cm&to=<%=openshare.email%>&su=<%=openshare.title%>&body=<%=openshare.description%>&bcc=<%=openshare.email%>&cc=<%=openshare.email%>"
+                "data": "view=cm&to=<%=encodeURIComponent(openshare.email)%>&su=<%=encodeURIComponent(openshare.title)%>&body=<%=encodeURIComponent(openshare.description)%>&bcc=<%=encodeURIComponent(openshare.email)%>&cc=<%=encodeURIComponent(openshare.email)%>"
             },
             "yahoo": {
                 "type": "redirect",
                 "url": "http://compose.mail.yahoo.com/",
-                "data": "to=<%=openshare.email%>&subject=<%=openshare.title%>&body=<%=openshare.description%>"
+                "data": "to=<%=encodeURIComponent(openshare.email)%>&subject=<%=encodeURIComponent(openshare.title)%>&body=<%=encodeURIComponent(openshare.description)%>"
             },
             "wechat": {
                 "type": "native",
@@ -428,10 +428,9 @@
          */
         getShareData: function(type, key){
             var data = null;
+            var sd = this.shareData[type] || {};
 
-            if(type in this.shareData){
-                data = $.extend({}, this.getBasicShareData(), this.shareData[type][(key || this.namespace)] || null);
-            }
+            data = $.extend({}, this.getBasicShareData(), sd[(key || this.namespace)] || null);
 
             return this.og(data, type);
         },
